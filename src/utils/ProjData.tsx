ebc84b1b79302fcd80b8a44d9296596410ddfacb
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-12-07 15:28:37 +0530

    Add more data

--- src/utils/ProjData.tsx
+++ src/utils/ProjData.tsx
@@ -1,7 +1,87 @@
-import { projectTools as tools } from "./ToolData";
+import toolData from "./ToolData";
 import kinoflow from "../assets/proj/kinoflow.jpg";
 
 const projectData = [
+  {
+    name: "Unifyr",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    livelink: "https://unifyr.vercel.app/",
+    github: "https://github.com/tejasnasa/unifyr",
+    image: "https://via.placeholder.com/300x300?text=unifyr",
+    tools: [
+      toolData.ts,
+      toolData.nodejs,
+      toolData.postgres,
+      toolData.prisma,
+      toolData.nextjs,
+      toolData.tailwind,
+    ],
+  },
+  {
+    name: "Budgex",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    livelink: "https://budgex.vercel.app/",
+    github: "https://github.com/tejasnasa/budgex",
+    image: "https://via.placeholder.com/300x300?text=budgex",
+    tools: [
+      toolData.ts,
+      toolData.react,
+      toolData.tailwind,
+      toolData.sqlite,
+      toolData.drizzle,
+      toolData.vite,
+    ],
+  },
+  {
+    name: "Untitled E-Commerce Application",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "#",
+    image: "https://via.placeholder.com/300x300?text=ecommerce",
+    tools: [
+      toolData.ts,
+      toolData.react,
+      toolData.redux,
+      toolData.betterauth,
+      toolData.neon,
+      toolData.drizzle,
+    ],
+  },
+  {
+    name: "Untitled Communication App",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "#",
+    image: "https://via.placeholder.com/300x300?text=communication",
+    tools: [
+      toolData.ts,
+      toolData.socketio,
+      toolData.postgres,
+      toolData.prisma,
+      toolData.nextjs,
+      toolData.authjs,
+    ],
+  },
+  {
+    name: "Tejix",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa/ai-chatbot",
+    livelink: "https://tejix.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [
+      toolData.ts,
+      toolData.groqai,
+      toolData.supabase,
+      toolData.postgres,
+      toolData.prisma,
+      toolData.react,
+    ],
+  },
   {
     name: "KinoFlow",
     description:
@@ -9,7 +89,94 @@ const projectData = [
     github: "https://github.com/tejasnasa",
     livelink: "https://kinoflow.vercel.app",
     image: kinoflow,
-    tools: [tools.bootstrap],
+    tools: [
+      toolData.js,
+      toolData.nodejs,
+      toolData.express,
+      toolData.bootstrap,
+      toolData.vercel,
+    ],
+  },
+  {
+    name: "Untitled Code Editor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
+  },
+  {
+    name: "Algorithm Visualiser",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [],
+  },
+  {
+    name: "YelpCamp",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
+  },
+  {
+    name: "Full Stack Todo List",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
+  },
+  {
+    name: "Full Stack Instagram Clone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
+  },
+  {
+    name: "Tic-Tac-Toe",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.ts, toolData.nextjs],
+  },
+  {
+    name: "Investment Calculator",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
+  },
+  {
+    name: "Login Demo",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
+  },
+  {
+    name: "Geek Room Portal Backend",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus culpa necessitatibus nobis labore praesentium odit, inventore non quasi molestias, rem et? In consequatur, laudantium alias necessitatibus eveniet qui. Doloribus.",
+    github: "https://github.com/tejasnasa",
+    livelink: "https://kinoflow.vercel.app",
+    image: "https://via.placeholder.com/300x300?text=tejix",
+    tools: [toolData.bootstrap, toolData.nodejs, toolData.express, toolData.js],
   },
 ];
 
