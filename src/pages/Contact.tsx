dae66d35aaf135a9ad78b6d270d3493359e862a5
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-12-13 19:09:35 +0530

    Create contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,33 @@
 import Message from "../components/contact/Message";
+import github from "../assets/contact/github.svg";
+import linkedin from "../assets/contact/linkedin.svg";
 
 const Contact = () => {
   return (
-    <section className="w-[84%] h-dvh mx-auto mt-24 text-white flex">
-        <Message />
+    <section className="w-[84%] h-dvh mx-auto mt-24 text-white flex justify-between">
+      <Message />
+      <div className="p-16 w-[48%]">
+        <a
+          href="mailto:tejasnasa2005@gmail.com"
+          className="rounded-[32px] dark:bg-[#1F2937] bg-white"
+        >
+          tejasnasa2005@gmail.com
+        </a>
+        <div>
+          <a
+            href="https://github.com/tejasnasa"
+            className="rounded-[32px] dark:bg-[#1F2937] bg-white"
+          >
+            <img src={github} alt="GITHUB" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tejasnasa/"
+            className="rounded-[32px] p-16 dark:bg-[#1F2937] bg-white"
+          >
+            <img src={linkedin} alt="LINKEDIN" />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
