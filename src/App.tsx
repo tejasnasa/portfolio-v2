47857dd0bf76e94eec065f39662e1485b480649a
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-04-07 23:01:16 +0530

    Fix more bugs

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
